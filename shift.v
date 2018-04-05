3ce985d776a8ac9b27bede9945462ad05faf2b3f
fullofquarks <nas150630@utdallas.edu>
2018-04-04 20:30:41 -0500

    Added makeshift testbench

--- shift.v
+++ shift.v
@@ -1,6 +1,7 @@
 module shift (clk, clr, si, so);
-    input clk, si, clr;
-    output so;
+    input clk,  clr;
+    input si;//[7:0];
+    output so [7:0];
     
     reg [7:0] tmp;
     always @(posedge clk or posedge clr)
@@ -8,7 +9,39 @@ module shift (clk, clr, si, so);
         if (clr)
            tmp <= 8'b00000000;
         else
-           tmp <= {si, tmp[6:0]};
+           //tmp <= {si, tmp[6:0]};
+            tmp = tmp >> 1;
     end
-    assign so = tmp[7];
+    assign so = tmp[0];
+endmodule
+
+module testbench;
+reg clk;
+reg clr;
+reg si;
+wire [7:0] so;
+
+shift shft(.clk(clk), .clr(clr), .si(si), .so(so));
+
+initial begin
+	clk = 0;
+	clr = 0;
+	si = 1'b1;
+	#5 $display("Serial in:%0b Clock:%0b", so, clk);
+	clr = 1;
+	#5 $display("Serial in:%0b Clock:%0b", so, clk);
+	clr = 0;
+	#5 $display("Serial in:%0b Clock:%0b", so, clk);
+	#5 $display("Serial in:%0b Clock:%0b", so, clk);
+	#5 $display("Serial in:%0b Clock:%0b", so, clk);
+	#5 $display("Serial in:%0b Clock:%0b", so, clk);
+	#5 $display("Serial in:%0b Clock:%0b", so, clk);
+	#5 $display("Serial in:%0b Clock:%0b", so, clk);
+	#5 $display("Serial in:%0b Clock:%0b", so, clk);
+	#5 $display("Serial in:%0b Clock:%0b", so, clk);
+	#5 $display("Serial in:%0b Clock:%0b", so, clk);
+	$finish;
+end
+
+always #5 clk = ~clk;
 endmodule
