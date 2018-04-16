8c5b7876a109ccbd9b4612d1a273054ad87e5226
Jacob Greenway <jagreenway12@gmail.com>
2018-04-16 03:06:13 -0500

    added 8bit adder

--- addersub.v
+++ addersub.v
@@ -1,12 +1,4 @@
-module ByteAdder;
-  
-endmodule
-
-module BtyeSub;
-
-endmodule
-
-module FullAdder(A, B, Cin, Sum, Cout);
+module FullAdder (A, B, Cin, Sum, Cout);
   input A;
   input B;
   input Cin;
@@ -22,6 +14,34 @@ module FullAdder(A, B, Cin, Sum, Cout);
   or(Cout, And1, And2);
 endmodule
 
+module ByteAdder (A, B, Cin, out, Cout);
+  // 8-bit inputs
+  input [7:0] A;
+  input [7:0] B;
+
+  input Cin;
+
+  // 8-bit output
+  output [7:0] out;
+
+  output Cout;
+
+  wire c1, c2, c3, c4, c5, c6, c7;
+
+  FullAdder fa0(A[0], B[0], Cin, out[0], c1);
+  FullAdder fa1(A[1], B[1], c1, out[1], c2);
+  FullAdder fa2(A[2], B[2], c2, out[2], c3);
+  FullAdder fa3(A[3], B[3], c3, out[3], c4);
+  FullAdder fa4(A[4], B[4], c4, out[4], c5);
+  FullAdder fa5(A[5], B[5], c5, out[5], c6);
+  FullAdder fa6(A[6], B[6], c6, out[6], c7);
+  FullAdder fa7(A[7], B[7], c7, out[7], Cout);
+endmodule
+
+module BtyeSub;
+
+endmodule
+
 module testbench;
 
   reg clock;
@@ -29,11 +49,12 @@ module testbench;
   reg [7:0] A;
   reg [7:0] B;
 
-  reg FAa, FAb, FAcin;
+  reg Cin;
 
-  wire FAsum, FAcout;
+  wire Cout;
+  wire [7:0] Sum;
 
-  FullAdder fullAdder(FAa, FAb, FAcin, FAsum, FAcout);
+  ByteAdder byteAdder(A, B, Cin, Sum, Cout);
 
   initial begin
     #1
@@ -43,15 +64,11 @@ module testbench;
     B[7:0] = 8'b11111111;
 
     clock=0;
-    clock=1; FAa=1; FAb=0; FAcin=0; display;
-    clock=0;
-    clock=1; FAa=1; FAb=1; FAcin=1; display;
-    clock=0;
-    clock=1; FAa=0; FAb=0; FAcin=1; display;
+    clock=1; Cin=0; display;
   end
 
   task display;
-    #1 $display("Clock: %b | A + B: %b Cout: %b", clock, FAsum, FAcout);
+    #1 $display("Clock: %b | Cin: %b | A + B: %b | Cout: %b", clock, Cin, Sum, Cout);
   endtask
 
 endmodule
